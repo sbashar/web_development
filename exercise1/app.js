eee0836792101542ff0d44f2dba27cb79ca5a183
Saydul Bashar <tamimnoone@gmail.com>
2016-08-16 14:47:53 +0000

    Added some examples for execution context.

--- exercise1/app.js
+++ exercise1/app.js
@@ -1,8 +1,29 @@
-b();
+var a;
 console.log(a);
-var a = 'Hello World!';
 
-function b() {
-    console.log('Called b!');
+if (a === undefined) {
+    console.log('a is undefined!');
+}
+else {
+    console.log('a is defined!');
 }
 
+function b() {
+    console.log('Caller b!');
+}
+
+b();
+
+console.log(c);
+
+var c = 'Hello World!';
+
+console.log(c);
+
+
+function d() {
+    console.log('Caller d!');
+    b();
+}
+
+d();
